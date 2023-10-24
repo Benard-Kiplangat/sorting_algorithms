155d4390e990688f59c51737671fceb63fb80015
benard Kiplangat <benardkiplangat97@gmail.com>
2023-10-24 11:42:20 +0300

    Implementation of selection sort algorithim on an array of integers

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * selection_sort - a function that sorts an array of integers in
+ * ascending order using selection sort algorithim
+ *
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned long int i, j, k;
+	int min;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		j = i + 1;
+		k = i;
+		while (j < size)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				k = j;
+			}
+			j++;
+		}
+		j--;
+		if (i != j)
+		{
+			array[k] = array[i];
+			array[i] = min;
+		}
+		print_array(array, size);
+	}
+}
